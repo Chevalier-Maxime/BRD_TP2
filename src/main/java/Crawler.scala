3c2959adee359cb59233382bda8b7eebd524b964
Chevalier-Maxime <chevalier.maxime@outlook.com>
2017-11-21 09:36:38 -0500

    crawler en cour, ajouter spell (see TODO)

--- src/main/java/Crawler.scala
+++ src/main/java/Crawler.scala
@@ -8,20 +8,58 @@ import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 import scala.collection.JavaConversions._
+import scala.collection.mutable.ListBuffer
 
 class Crawler (URL: URL){
 
+  def processBlock(elementsBetween: ListBuffer[Elements]) = {
+
+  }
+
   def crawlerCreaturePage(e: Element) = {
+    //TODO différencier les pages avec un seul monstre et celles avec plusieurs. (via get parent ?) ou via stat-block-title (dans le HTML)
+    //utiliser ca : https://stackoverflow.com/questions/6534456/jsoup-how-to-get-all-html-between-2-header-tags
     val url = e.child(0).attr("abs:href")
     val doc = Jsoup.connect(url).get()
-    val elements = doc.select("div.body").first()
-    val nom = elements.select("p.stat-block-title").first().child(0).childNode(0).toString
-    val creature = new Creature(nom)
 
-    for(child <- elements.select("a[href]")){
-      if(child.attr("href").contains("/spells/"))
-        System.out.println("Blop") //TODO On a le lien du spell, trouver son nom et ajouter a la créature.
+    val firstBlock = doc.select("p.stat-block-title").first()
+    val siblings = firstBlock.siblingElements()
+    var elementsBetween : ListBuffer[Elements] = ListBuffer()
+
+    var trouve : Boolean = false;
+    for ( i <-1 to siblings.size()) {
+      var sibling = siblings.get(i)
+      if (!((sibling.tag().equals("p")) && (sibling.attr("class").contains("stat-block-title"))) && trouve) {
+        sibling +: elementsBetween
+      } else {
+        if (trouve) {
+          processBlock(elementsBetween)
+          elementsBetween = ListBuffer()
+        } else {
+          if ((sibling.attr("class").contains("stat-block-title")) && !trouve) {
+            elementsBetween = ListBuffer()
+          }
+        }
+      }
     }
+    if(!elementsBetween.isEmpty){
+      processBlock(elementsBetween)
+    }
+
+
+
+//    val elements = doc.select("div.body").first()
+//    val nom = elements.select("p.stat-block-title").first().child(0).childNode(0).toString
+//    val creature = new Creature(nom)
+//
+//    for(child <- elements.select("a[href]")){
+//      if(child.attr("href").contains("/spells/")){
+//        System.out.println("Blop") //TODO On a le lien du spell, trouver son nom et ajouter a la créature.
+//      }
+//    }
+
+
+
   }
 
   def crawlerOnThisLetter(elements: Elements) = {
